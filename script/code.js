2721b086e000dd74a1c43fbd3945d00644ed7d7c
JCGViljoen <jcgviljoen51@gmail.com>
2023-05-28 18:39:07 +0200

    task complete

---
+++ script/code.js
@@ -0,0 +1,17 @@
+function greet(Hello){
+    console.log(greet);
+    return('Hello')
+}
+
+greet()
+
+function multiply(a, b){
+    let result = a * b
+    console.log('${a} multiplied by ${b} equals ${result}' )
+}
+
+function tableOfFive(variable){
+   console.log(variable * 5);
+   return (variable * 5)
+    
+}
